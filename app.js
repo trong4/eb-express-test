a37fc8593264f841f64a7dbeceae584d94aecf37
trong <devgruxcon@gmail.com>
2016-12-27 15:24:41 +0000

    test error recovery 3

--- app.js
+++ app.js
@@ -49,10 +49,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-setTimeout(function(){
-    throw Error("mach remove. testing error on EB")
-}, 15000)
-
 setTimeout(function(){ // mach remove
     process.exit(1)
-}, 30000)
+}, 10000)
